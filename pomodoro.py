3e202aef2037e6ef7549589afb2595b8e7b9fb70
Arcibashev <72345107+GrigoryArcibashev@users.noreply.github.com>
2021-12-01 15:31:46 +0500

    Update pomodoro.py

--- pomodoro.py
+++ pomodoro.py
@@ -1,33 +1,61 @@
-from time import sleep
-from winsound import Beep
+import os
+import sys
+import time
+import winsound
+
+
+SHORT_REST_TIME = 5 * 60
+LONG_REST_TIME = 30 * 60
+WORK_TIME = 25 * 60
 
 
 def start_timer(seconds: int):
-    while seconds >= 0:
-        print(f'\r>>> {seconds // 60:02} : {seconds % 60:02}', end='')
-        sleep(1)
+    print(f'\r>>> {seconds // 60:02} : {seconds % 60:02}', end='')
+    while seconds > 0:
+        time.sleep(1)
         seconds -= 1
-
-
-def beep():
-    Beep(2500, 1000)
+        print(f'\r>>> {seconds // 60:02} : {seconds % 60:02}', end='')
 
 
 def cycle(rest_in_sec: int):
-    start_timer(25 * 60)
+    start_timer(WORK_TIME)
     beep()
     print('\n>>> Rest')
     start_timer(rest_in_sec)
     beep()
 
 
+def beep():
+    winsound.Beep(2500, 1000)
+
+
+def clear_screen():
+    os.system('CLS')
+
+
+def process_user_input():
+    inp = str()
+    while inp not in ('y', 'n'):
+        inp = input('\n>>> Do you want to start the timer? (Y/N): ')
+        if inp.isalpha():
+            inp = inp.lower()
+    if inp == 'y':
+        clear_screen()
+    else:
+        shut_down()
+
+
+def shut_down():
+    print('Нажмите любую кнопку, чтобы завершить программу')
+    input()
+    sys.exit(0)
+
+
 def main():
     cycle_count = 1
     while True:
-        inp = str()
-        while inp.lower() != 'y':
-            inp = input('\n>>> Do you want to start the timer? (Y): ')
-        cycle(5 * 60 if cycle_count != 0 else 30 * 60)
+        process_user_input()
+        cycle(SHORT_REST_TIME if cycle_count != 0 else LONG_REST_TIME)
         cycle_count = (cycle_count + 1) % 4
 
 
